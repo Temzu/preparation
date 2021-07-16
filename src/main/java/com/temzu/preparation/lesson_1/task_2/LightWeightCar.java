c29e4700421a806adda5ecfaad35ad0f1f815433
temzu <artem-zubkov-dev@inbox.ru>
2021-07-16 14:16:06 +0300

    task_2

---
+++ src/main/java/com/temzu/preparation/lesson_1/task_2/LightWeightCar.java
@@ -0,0 +1,27 @@
+package com.temzu.preparation.lesson_1.task_2;
+
+public class LightWeightCar extends Car {
+    public LightWeightCar(Engine engine, String color, String name) {
+        super(engine, color, name);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
